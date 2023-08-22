4242b3ec12fadcf0b219438ed0330a3db7a982d7
BCaneteBalestrelli <benjaminbalestrelli@gmail.com>
2023-08-22 20:05:25 -0300

    Lo último esta en Routes.js

--- api/src/models/Temperaments.js
+++ api/src/models/Temperaments.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('temperament', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
@@ -11,19 +11,6 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      validate: { isUrl: true },
-    },
-    height: {
-      type: DataTypes.STRING,
-    },
-    weight: {
-      type: DataTypes.STRING,
-    },
-    lifeExpectance: {
-      type: DataTypes.STRING,
     },
 
   });
---
+++ api/src/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/dogs',);
+router.get('/dogs/:idRaza',);
+router.get('/dogs/name?="..."',);
+router.post('/dogs',);
+router.get('temperaments',);
